2a16b4576f413be6cf98ca0ce40f512bb414cf92
Mohammad Amin Zare Soltani <48747618+mazsoltani@users.noreply.github.com>
2024-03-16 15:53:36 +0330

    Add files via upload

---
+++ Session2/3- Requests module/main.py
@@ -0,0 +1,11 @@
+import requests
+
+print("Version: ", requests.__version__)
+print("Licence: ", requests.__license__)
+print("Copyright: ", requests.__copyright__)
+print("Author:", requests.__author__)
+print("Author email:", requests.__author_email__)
+print("Document url, title, description:")
+print(f"Title -> {requests.__title__}")
+print(f"URL -> {requests.__url__}")
+print(f"Description -> {requests.__description__}")
